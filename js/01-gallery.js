983a1bb6d95c35bd770f43be9ef1ea4a1f6df9bd
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-09-13 20:14:15 +0300

    1st task done but have to fix bug

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMainBlockRef = document.querySelector('.gallery');
+// console.log(galleryMainBlockRef);
+
+galleryMainBlockRef.addEventListener('click', onItemClick);
+
+function doItemsGalleryMarkup(objectImages) {
+  const galleryItems = objectImages
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+  return galleryItems;
+}
+
+galleryMainBlockRef.insertAdjacentHTML('beforeend', doItemsGalleryMarkup(galleryItems));
+
+function onItemClick(event) {
+  event.preventDefault();
+  const elem = event.target;
+
+  if (elem.nodeName !== 'IMG') {
+    return;
+  }
+
+  //   console.dir(elem);
+
+  const instance = basicLightbox.create(`<img src="${elem.dataset.source}">`);
+  //   const instance = basicLightbox.create(`${elem}`);
+  instance.show();
+
+  //   const openedImage = document.querySelector('.basicLightbox');
+  //   console.log(openedImage);
+
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+      console.log(event.code);
+    }
+  });
+}
+
+// function closeOnEscape(event) {
+//   console.log(event.code);
+//   if (event.code === 'Escape') {
+//     instance.close();
+//     openedImage.removeEventListener('keydown', closeOnEscape);
+//   }
+// }
